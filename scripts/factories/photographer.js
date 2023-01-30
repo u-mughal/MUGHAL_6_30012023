c1fddd862ab44bd6916e54191803e8b6281b4e55
Usman <usman.mughal@outlook.fr>
2023-01-30 17:46:01 +0100

    importations des datas json

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -1,13 +1,16 @@
 function photographerFactory(data) {
-    const { name, portrait } = data;
+    const { id, portrait, name, city, country, tagline, price, totalLikes } =
+        data;
+
+
 
     const picture = `assets/photographers/${portrait}`;
 
     function getUserCardDOM() {
-        const article = document.createElement( 'article' );
-        const img = document.createElement( 'img' );
+        const article = document.createElement('article');
+        const img = document.createElement('img');
         img.setAttribute("src", picture)
-        const h2 = document.createElement( 'h2' );
+        const h2 = document.createElement('h2');
         h2.textContent = name;
         article.appendChild(img);
         article.appendChild(h2);
