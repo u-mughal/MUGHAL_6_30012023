ebbe466e43f6171cfa24b9bb0f5ac57b956d07b9
Usman <usman.mughal@outlook.fr>
2023-01-31 17:22:23 +0100

    ajt des éléments nécessaires pour chaque card de photographe

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -1,21 +1,45 @@
 //Création d'un objet photographe avec les propeétés extraites à partir des données fournies.
 function photographerFactory(data) {
-    const { id, portrait, name, city, country, tagline, price, totalLikes } =
-        data;
-
-
-
+    const { id, portrait, name, city, country, tagline, price, totalLikes } = data;
     const picture = `assets/photographers/${portrait}`;
 
     function getUserCardDOM() {
         const article = document.createElement('article');
+        article.setAttribute("class", "photographer-card");
+
         const img = document.createElement('img');
-        img.setAttribute("src", picture)
+        img.setAttribute("src", picture);
+        img.setAttribute("alt", `Portrait de ${name}`);
+
         const h2 = document.createElement('h2');
         h2.textContent = name;
+
+        const cityElement = document.createElement('p');
+        cityElement.textContent = `Ville : ${city}`;
+
+        const countryElement = document.createElement('p');
+        countryElement.textContent = `Pays : ${country}`;
+
+        const taglineElement = document.createElement('p');
+        taglineElement.textContent = tagline;
+
+        const priceElement = document.createElement('p');
+        priceElement.textContent = `Prix : ${price}`;
+
+        const totalLikesElement = document.createElement('p');
+        totalLikesElement.textContent = `Nombre de likes : ${totalLikes}`;
+
         article.appendChild(img);
         article.appendChild(h2);
-        return (article);
+        article.appendChild(cityElement);
+        article.appendChild(countryElement);
+        article.appendChild(taglineElement);
+        article.appendChild(priceElement);
+        article.appendChild(totalLikesElement);
+
+        return article;
     }
-    return { name, picture, getUserCardDOM }
+
+    return { name, picture, city, country, tagline, price, totalLikes, getUserCardDOM };
 }
+
