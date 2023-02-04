b34a46efe4eea6afcb7706fae9f3c3098bc8f826
Usman <usman.mughal@outlook.fr>
2023-02-04 19:51:10 +0100

    function création de carte photographe

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -1,45 +1,41 @@
-//Création d'un objet photographe avec les propeétés extraites à partir des données fournies.
 function photographerFactory(data) {
     const { id, portrait, name, city, country, tagline, price, totalLikes } = data;
-    const picture = `assets/photographers/${portrait}`;
+    const picture = `./assets/photographers/${portrait}`;
 
-    function getUserCardDOM() {
+
+    // Function création de carte photographe 
+    function generatePhotographerCard() {
         const article = document.createElement('article');
-        article.setAttribute("class", "photographer-card");
+        article.setAttribute("id", id);
+
+        const link = document.createElement('a');
+        link.setAttribute("href", `./photographer.html?id=${id}`);
 
         const img = document.createElement('img');
         img.setAttribute("src", picture);
-        img.setAttribute("alt", `Portrait de ${name}`);
+        img.setAttribute("alt", name);
 
         const h2 = document.createElement('h2');
         h2.textContent = name;
 
-        const cityElement = document.createElement('p');
-        cityElement.textContent = `Ville : ${city}`;
+        link.appendChild(img);
+        link.appendChild(h2);
+        article.appendChild(link);
 
-        const countryElement = document.createElement('p');
-        countryElement.textContent = `Pays : ${country}`;
+        const cityCountry = document.createElement('span');
+        cityCountry.textContent = `${city}, ${country}`;
+        article.appendChild(cityCountry);
 
-        const taglineElement = document.createElement('p');
-        taglineElement.textContent = tagline;
+        const taglineStrong = document.createElement('strong');
+        taglineStrong.textContent = tagline;
+        article.appendChild(taglineStrong);
 
         const priceElement = document.createElement('p');
-        priceElement.textContent = `Prix : ${price}`;
-
-        const totalLikesElement = document.createElement('p');
-        totalLikesElement.textContent = `Nombre de likes : ${totalLikes}`;
-
-        article.appendChild(img);
-        article.appendChild(h2);
-        article.appendChild(cityElement);
-        article.appendChild(countryElement);
-        article.appendChild(taglineElement);
+        priceElement.textContent = `${price}€/jour`;
         article.appendChild(priceElement);
-        article.appendChild(totalLikesElement);
 
         return article;
     }
 
-    return { name, picture, city, country, tagline, price, totalLikes, getUserCardDOM };
+    return { generatePhotographerCard };
 }
-
