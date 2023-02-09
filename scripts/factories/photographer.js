a0dc872c4eaf0039e21750a69d8eb39cdc5af50e
Usman <usman.mughal@outlook.fr>
2023-02-09 16:19:21 +0100

    maj création d'article

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -1,46 +1,35 @@
 function photographerFactory(data) {
+    // Décomposition des propriétés de l'objet data
     const { id, portrait, name, city, country, tagline, price, totalLikes } = data;
+
+    // Définition de la source de l'image
     const picture = `./assets/photographers/${portrait}`;
 
     // Fonction pour créer la carte d'un photographe 
     function generatePhotographerCard() {
+        // Création de l'élément HTML article
         const article = document.createElement('article');
+
+        // Attribution de l'ID à l'article
         article.setAttribute("id", id);
 
-        // Utilisation de la méthode searchParams pour définir le lien
-        const params = new URL(document.location).searchParams;
-        params.set("id", id);
-        const href = `./photographer.html?${params}`;
+        // Insertion du HTML à l'intérieur de l'article
+        article.insertAdjacentHTML("beforeend",
+            `
+            <a href="${`./photographer.html?${id}`}">
+                <img src="${picture}" alt="${name}">
+                <h2>${name}</h2>
+            </a>
+            <span>${city}, ${country}</span><br>
+            <strong>${tagline}</strong><br>
+            ${price}€/jour
+            `
+        );
 
-        const link = document.createElement('a');
-        link.setAttribute("href", href);
-
-        const img = document.createElement('img');
-        img.setAttribute("src", picture);
-        img.setAttribute("alt", name);
-
-        const h2 = document.createElement('h2');
-        h2.textContent = name;
-
-        link.appendChild(img);
-        link.appendChild(h2);
-        article.appendChild(link);
-
-        const cityCountry = document.createElement('span');
-        cityCountry.textContent = `${city}, ${country}`;
-        article.appendChild(cityCountry);
-
-        const taglineStrong = document.createElement('strong');
-        taglineStrong.textContent = tagline;
-        article.appendChild(taglineStrong);
-
-        const priceElement = document.createElement('p');
-        priceElement.textContent = `${price}€/jour`;
-        article.appendChild(priceElement);
-
+        // Retour de l'élément article
         return article;
     }
 
+    // Retour de l'objet avec la fonction generatePhotographerCard
     return { generatePhotographerCard };
-
 }
