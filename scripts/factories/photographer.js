15df342e23b1a9b4e50e92071ee8a03cb5ff5fdf
Usman <usman.mughal@outlook.fr>
2023-02-12 17:25:28 +0100

    màj insertAdjacentHTML

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -1,35 +1,36 @@
 function photographerFactory(data) {
-    // Décomposition des propriétés de l'objet data
-    const { id, portrait, name, city, country, tagline, price, totalLikes } = data;
+  // Décomposition des propriétés de l'objet data
+  const { id, portrait, name, city, country, tagline, price, totalLikes } = data;
 
-    // Définition de la source de l'image
-    const picture = `./assets/photographers/${portrait}`;
+  // Fonction pour créer la carte d'un photographe 
+  function generatePhotographerCard() {
+    // Création de l'élément HTML article
+    const article = document.createElement('article');
 
-    // Fonction pour créer la carte d'un photographe 
-    function generatePhotographerCard() {
-        // Création de l'élément HTML article
-        const article = document.createElement('article');
+    // Attribution de l'ID à l'article
+    article.setAttribute("id", id);
 
-        // Attribution de l'ID à l'article
-        article.setAttribute("id", id);
+    // Insertion du HTML à l'intérieur de l'article
+    article.insertAdjacentHTML(
+      "beforeend",
+      `
+      <a href="./photographer.html?id=${id}">
+          <img
+              alt="${name}"
+              src="./assets/photographers/${portrait}"
+          />
+          <h2>${name}</h2>
+      </a>
+      <span>${city}, ${country}</span><br />
+      <strong>${tagline}</strong><br />
+      ${price}€/day
+      `
+    );
 
-        // Insertion du HTML à l'intérieur de l'article
-        article.insertAdjacentHTML("beforeend",
-            `
-            <a href="${`./photographer.html?${id}`}">
-                <img src="${picture}" alt="${name}">
-                <h2>${name}</h2>
-            </a>
-            <span>${city}, ${country}</span><br>
-            <strong>${tagline}</strong><br>
-            ${price}€/jour
-            `
-        );
+    // Retour de l'élément article
+    return article;
+  }
 
-        // Retour de l'élément article
-        return article;
-    }
-
-    // Retour de l'objet avec la fonction generatePhotographerCard
-    return { generatePhotographerCard };
+  // Retour de l'objet avec la fonction generatePhotographerCard
+  return { generatePhotographerCard };
 }
