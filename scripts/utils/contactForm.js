b44d1e130fb65becb1bbf117cf600e856751991b
Usman <usman.mughal@outlook.fr>
2023-02-22 19:31:28 +0100

    création fonction afficher/fermer la modale

--- scripts/utils/contactForm.js
+++ scripts/utils/contactForm.js
@@ -1,9 +1,30 @@
+// Récupération des éléments HTML nécessaires
+const mainPhotographer = document.getElementById("main-photographer");
+const form = document.forms.contact;
+const modal = document.getElementById("contact_modal");
+const modalCloseBtn = document.querySelector(".close-modal");
+const modalOpenBtn = document.querySelector("#btn_open_modal");
+const allInput = [...document.querySelectorAll("input")];
+allInput[3] = document.querySelector("#request");
+
+// Fonction pour afficher la fenêtre modale
 function displayModal() {
-    const modal = document.getElementById("contact_modal");
-	modal.style.display = "block";
+    mainPhotographer.setAttribute("aria-hidden", "true");
+    mainPhotographer.classList.add("no-scroll");
+    modal.setAttribute("aria-hidden", "false");
+    modal.style.display = "block";
+    form.elements.first.focus();
 }
 
+// Fonction pour fermer la fenêtre modale
 function closeModal() {
-    const modal = document.getElementById("contact_modal");
+    const modalOpenBtn = document.querySelector("#btn_open_modal");
+    mainPhotographer.setAttribute("aria-hidden", "false");
+    mainPhotographer.classList.remove("no-scroll");
+    modal.setAttribute("aria-hidden", "true");
     modal.style.display = "none";
+    modalOpenBtn.focus();
+    hidethanks_field();
+    form.reset();
 }
+
