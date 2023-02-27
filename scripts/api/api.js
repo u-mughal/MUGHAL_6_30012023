a621161792938df1810898b8e575fbcce1936590
Usman <usman.mughal@outlook.fr>
2023-02-27 19:31:46 +0100

    Fonction getMediaById" qui filtre les données + ajout propriété getMedia

--- scripts/api/api.js
+++ scripts/api/api.js
@@ -18,11 +18,22 @@ function api(url) {
             .catch(err => console.log("An error occurs:", err)); // Gestion des erreurs
     }
 
+    // Fonction asynchrone "getMediaById" qui filtre les données d'un type spécifié par ID
+    async function getMediaById(type, id) {
+        // Utilisation de la méthode Fetch pour récupérer les données à partir de l'URL spécifiée
+        return fetch(url)
+            .then(res => res.json()) // Conversion des données en format JSON
+            .then(res => res[type].filter(item => item.photographerId === parseInt(id))) // Filtrage des données
+            .catch(err => console.log("An error occurs:", err)); // Gestion des erreurs
+    }
+
     // Déclaration d'un objet qui sera retourné à la fin de la fonction api
     return {
         // Propriété getPhotographers qui retourne un appel à la fonction get avec le paramètre "photographers"
         getPhotographers: () => get("photographers"),
         // Propriété getPhotographer qui retourne un appel à la fonction get avec les paramètres "photographers" et id
         getPhotographer: (id) => get("photographers", id),
+        // Propriété getMedia qui retourne un appel à la fonction getMediaById avec les paramètres "media" et id
+        getMedia: (id) => getMediaById("media", id),
     };
 }
