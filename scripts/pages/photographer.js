f71e1da61f5bba94ed17f456fbd77607906f4005
Usman <usman.mughal@outlook.fr>
2023-03-01 18:17:26 +0100

    création portfolio header + function setContactName du modale

--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -1,19 +1,54 @@
-// Initialisation de la page détail du photographe
-async function init() {
-    // Récupération des données du photographe en utilisant la fonction api avec le chemin vers le fichier JSON des photographes
+// Cette fonction est exécutée lorsque le contenu de la page est chargé et appelle les fonctions pour récupérer les données du photographe et les médias, fusionner les données et afficher le portfolio.
+document.addEventListener("DOMContentLoaded", async (event) => {
+    const dataPhotographer = await getDataPhotographer();
+    const copyPhotographerData = clonePhotographer(dataPhotographer);
+
+    setContactName(dataPhotographer);
+    displayPortfolioHeader(copyPhotographerData);
+});
+
+// Cette fonction utilise l'API pour récupérer les données du photographe en fonction de l'ID présent dans l'URL et renvoie un objet photographe.
+async function getDataPhotographer() {
     const data = api("./data/photographers.json");
-    // Récupération du photographe en fonction de l'ID passé en paramètre dans l'URL
     const photographer = await data.getPhotographer(getParamId());
-    // Récupération de l'élément HTML qui sera le container pour l'entête du photographe
-    const header = document.querySelector(".photograph-header");
-    // Utilisation de la factory function pour générer l'entête du photographe
-    const factory = photographerFactory(photographer);
-    // Ajout de l'entête du photographe dans le container HTML
-    header.appendChild(factory.generatePhotographerHeader());
+    return photographer;
 }
 
-// Ajout d'un écouteur d'événement pour le contenu du DOM
-document.addEventListener("DOMContentLoaded", async () => {
-    // Attends l'exécution de la fonction init
-    await init();
-});
+// Cette fonction utilise l'API pour récupérer les médias du photographe en fonction de l'ID présent dans l'URL et renvoie un tableau de médias.
+async function getMedia() {
+    const data = api("./data/photographers.json");
+    const media = await data.getMedia(getParamId());
+    return media;
+}
+
+// Cette fonction renvoie une copie de l'objet photographe pour éviter de modifier l'objet original.
+function clonePhotographer(photographer) {
+    return { ...photographer };
+}
+
+// Cette fonction fusionne le nom du photographe avec chaque média pour créer un tableau de médias fusionnés.
+function mergeMedia(photographer, media) {
+    const name = { name: photographer.name.split(" ")[0] };
+    let mediaMerge = [];
+    media.forEach((element) => {
+        mediaMerge.push({ ...name, ...element });
+    });
+    return mediaMerge;
+}
+
+// Cette fonction modifie le nom du titre de la modale pour inclure le nom du photographe.
+function setContactName(photographer) {
+    const modal = document.getElementById("contact_modal");
+    modal.setAttribute("aria-describedby", `contactez ${photographer.name}`);
+    const modalTitle = document.querySelector(".modal header h2");
+    modalTitle.setAttribute("id", `contactez ${photographer.name}`);
+    modalTitle.innerHTML = `Contactez-moi<br/> ${photographer.name}`;
+}
+
+// Cette fonction affiche le header du photographe à partir de l'objet photographe.
+function displayPortfolioHeader(photographer) {
+    const wrapper = document.querySelector(".photograph-header");
+    const factoryPhotographer = photographerFactory(photographer);
+    const headerPhotographer = factoryPhotographer.generatePhotographerHeader();
+    wrapper.appendChild(headerPhotographer);
+}
